312cccc57e5380a1fbe989cf2c4a0bc7c5189d88
crawlive <crawfolivia@gmail.com>
2022-10-13 15:14:26 -0500

    Color validation is finished

--- src/main/java/org/example/Commands/Color.java
+++ src/main/java/org/example/Commands/Color.java
@@ -3,15 +3,32 @@ package org.example.Commands;
 import java.util.ArrayList;
 
 public class Color {
+
+    private static final String[] validColors = {"Red", "Blue", "Yellow", "Orange", "Green"};
+
     public static void color (ArrayList<String> command){
         if(command.get(0).equals("COLOR")) {
             String color = command.get(1);
-            changeColor(color);
+            if(validateColor(color)) {
+                changeColor(color);
+            } else {
+                System.out.println(color + " is an invalid color");
+            }
         }
     }
 
+    private static boolean validateColor(String color) {
+        boolean validation = false;
+        for (String validColor : validColors) {
+            if (validColor.equals(color)) {
+                validation = true;
+                break;
+            }
+        }
+        return validation;
+    }
+
     private static void changeColor(String color) {
-        //TODO: check for not-allowed colors
         System.out.println("Color Changed: " + color);
     }
 }
