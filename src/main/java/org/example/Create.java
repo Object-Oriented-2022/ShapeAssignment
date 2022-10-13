cd62fa46d60909aa87d3367d60f680a66628ded7
crawlive <crawfolivia@gmail.com>
2022-10-13 14:20:30 -0500

    Current File Creations

---
+++ src/main/java/org/example/Create.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Create {
+    public static void create(ArrayList<String> command){
+        System.out.print("Create Class");
+        if(command.get(0).equals("CREATE")) {
+            /*switch(COMMAND.GET(1)){
+                case "Rectangle":
+                    createRectangle();
+                    break;
+                case "Circle":
+                    createCircle();
+                    break;
+                default:
+                    System.out.println("Shape does note exist!");
+                    break;
+
+            }*/
+        }
+    }
+}
---
+++ src/main/java/org/example/EventHandler.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.util.ArrayList;
+
+public class EventHandler {
+
+    public static void handler(ArrayList<String>[] command){
+        System.out.println("Handler");
+
+    }
+}
